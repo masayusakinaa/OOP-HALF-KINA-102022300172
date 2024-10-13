14879be3b17dab3d420e86e8652f3797f72161a4
kinal <masayusakinah6@gmail.com>
2024-10-14 01:44:11 +0700

    masayukin

---
+++ MODUL 1/TPMODUL1_KINA/src/Pembelian.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Pembelian {
+
+    // pub
+    public static int pilihPenerbangan;
+    public static ArrayList<Penerbangan> dataPenerbangan;
+
+    public static void detailTiket(){
+        Penumpang.tampilDafarPenumpang();
+        String nomorPenerbangan = null;
+        for (Penerbangan x : dataPenerbangan){
+        
+            if (pilihPenerbangan == dataPenerbangan.indexOf(x)+1) {
+                nomorPenerbangan = x.getNomorPenerbangan();
+                x.tampilDaftarPenerbangan();
+            }
+        }
+        
+        if (nomorPenerbangan == null){
+            System.out.println("Penerbangan belum dipilih");
+        }
+
+    }
+
+    public static ArrayList<Penerbangan> daftarPenerbangan(){
+     
+        for (Penerbangan x : dataPenerbangan){
+            System.out.println();
+            System.out.println(dataPenerbangan.indexOf(x)+1);
+            x.tampilDaftarPenerbangan();
+        }
+
+        return dataPenerbangan;
+        }
+
+   
+    public static void beliTiket(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("NIK   :");
+        Penumpang.setNik(sc.nextLine());
+        System.out.print("NAMA DEPAN   :");
+        Penumpang.setNamaDepan(sc.nextLine());
+        System.out.print("NAMA BELAKANG   :");
+        Penumpang.setNamaBelakang(sc.nextLine());
+        Penumpang.tampilDafarPenumpang();
+
+        System.out.print("pilih nomor penerbangan");
+        pilihPenerbangan = sc.nextInt();
+
+        if (pilihPenerbangan > dataPenerbangan.size()) {
+            System.out.println("Penerbangan tidak tersedia");
+        }
+    
+    }
+
+    public static void main(String[] args) throws Exception {
+        dataPenerbangan = new ArrayList<>();
+        dataPenerbangan.add(new Penerbangan("GA101", "CGK, Jakarta", "DPS, Bali", "06.30", "08.15", 120000000));
+        dataPenerbangan.add(new Penerbangan("GA102", "CGK, Jakarta", "DPS, Bali", "06.30", "08.15", 120000000));
+        
+        Scanner sc = new Scanner(System.in);
+        int chooseMenu;
+
+    do { 
+        System.out.println();
+        System.out.println("Tampilin menu");
+        System.out.println("1 daftar penerbangan");
+        System.out.println("2 beli tiket");
+        System.out.println("3 tampilkan tiket");
+        System.out.println("4. exit");
+        chooseMenu = sc.nextInt();
+
+        switch (chooseMenu) {
+            case 1:
+                daftarPenerbangan();
+                break;
+            case 2:
+                beliTiket();
+                break;
+            case 3:
+                detailTiket();
+                break;
+            case 4:
+                System.out.println("Terima kasih.");
+                break;
+            default:
+                System.out.println("Menu tidak tersedia.");
+        }
+    } while (chooseMenu != 4);
+    
+    sc.close();
+    }
+
+}
+
+
---
+++ MODUL 1/TPMODUL1_KINA/src/Penumpang.java
@@ -0,0 +1,23 @@
+public class Penumpang {
+    private static String nik, namaDepan, namaBelakang;
+
+    public static void setNamaBelakang(String namaBelakang) {
+        Penumpang.namaBelakang = namaBelakang;
+    }
+
+    public static void setNamaDepan(String namaDepan) {
+        Penumpang.namaDepan = namaDepan;
+    }
+
+    public static void setNik(String nik) {
+        Penumpang.nik = nik;
+    }
+
+    
+    public static void tampilDafarPenumpang(){
+        System.out.println();
+        System.out.println("NIK : "+nik);
+        System.out.println("Nama Depan : "+namaDepan);
+        System.out.println("Nama Belakang : "+namaBelakang);
+    }
+}
