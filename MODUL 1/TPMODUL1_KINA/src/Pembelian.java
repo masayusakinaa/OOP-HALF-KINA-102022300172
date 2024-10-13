400285e5c36d585a0f1fe30641a054e86e316f9a
kinal <masayusakinah6@gmail.com>
2024-10-14 02:50:18 +0700

    kina

--- MODUL 1/TPMODUL1_KINA/src/Pembelian.java
+++ MODUL 1/TPMODUL1_KINA/src/Pembelian.java
@@ -11,7 +11,7 @@ public class Pembelian {
         Penumpang.tampilDafarPenumpang();
         String nomorPenerbangan = null;
         for (Penerbangan x : dataPenerbangan){
-        
+
             if (pilihPenerbangan == dataPenerbangan.indexOf(x)+1) {
                 nomorPenerbangan = x.getNomorPenerbangan();
                 x.tampilDaftarPenerbangan();
@@ -38,38 +38,39 @@ public class Pembelian {
    
     public static void beliTiket(){
         Scanner sc = new Scanner(System.in);
-        System.out.print("NIK            :");
+        System.out.print(s:"NIK            :");
         Penumpang.setNik(sc.nextLine());
-        System.out.print("NAMA DEPAN     :");
+        System.out.print(s:"NAMA DEPAN     :");
         Penumpang.setNamaDepan(sc.nextLine());
-        System.out.print("NAMA BELAKANG  :");
+        System.out.print(s:"NAMA BELAKANG  :");
         Penumpang.setNamaBelakang(sc.nextLine());
         Penumpang.tampilDafarPenumpang();
 
-        System.out.print("pilih nomor penerbangan");
+        System.out.print(s:"pilih nomor penerbangan");
         pilihPenerbangan = sc.nextInt();
 
         if (pilihPenerbangan > dataPenerbangan.size()) {
-            System.out.println("Penerbangan tidak tersedia");
+            System.out.println(x:"Penerbangan tidak tersedia");
         }
     
     }
 
     public static void main(String[] args) throws Exception {
         dataPenerbangan = new ArrayList<>();
-        dataPenerbangan.add(new Penerbangan("GA101", "CGK, Jakarta", "DPS, Bali", "06.30", "08.15", 120000000));
-        dataPenerbangan.add(new Penerbangan("GA102", "SUB, SURABAYA", "KNO, Medan", "09.30", "11.15", 135000000));
+        dataPenerbangan.add(new Penerbangan(nomorPenerbangan:"GA101", bandaraKeberangkatan:"CGK, Jakarta", bandaraKeberangkatan:"DPS, Bali", "06.30", "08.15", 120000000));
+        dataPenerbangan.add(new Penerbangan(nomorPenerbangan:"QZ202", bandaraKeberangkatan:"SUB, SURABAYA",bandaraKeberangkatan: "KNO, Medan", "09.00", "11.45", 135000000));
         
         Scanner sc = new Scanner(System.in);
         int chooseMenu;
 
     do { 
         System.out.println();
-        System.out.println("Tampilin menu");
-        System.out.println("1 daftar penerbangan");
-        System.out.println("2 beli tiket");
-        System.out.println("3 tampilkan tiket");
-        System.out.println("4. exit");
+        System.out.println("======= EAD Ticket Booking System =======");
+        System.out.println(x:"1. Tampilkan Daftar Penerbangan");
+        System.out.println(x:"2. Beli Tiket");
+        System.out.println(x:"3. Tampilkan Pesanan Tiket");
+        System.out.println(x:"4. Exit");
+        System.out.println(x:"Silahkan pilih menu: ");
         chooseMenu = sc.nextInt();
 
         switch (chooseMenu) {
@@ -81,12 +82,12 @@ public class Pembelian {
                 break;
             case 3:
                 detailTiket();
-                break;
+                
             case 4:
-                System.out.println("Terima kasih.");
+                System.out.println(x:"Terima kasih.");
                 break;
             default:
-                System.out.println("Menu tidak tersedia.");
+                System.out.println(x:"Menu tidak tersedia.");
         }
     } while (chooseMenu != 4);
     
