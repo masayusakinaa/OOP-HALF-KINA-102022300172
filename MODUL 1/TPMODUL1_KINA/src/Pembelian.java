5d7f545d69cf442c21dd4bc51911ac1985459c18
kinal <masayusakinah6@gmail.com>
2024-10-14 15:15:30 +0700

    kina

--- MODUL 1/TPMODUL1_KINA/src/Pembelian.java
+++ MODUL 1/TPMODUL1_KINA/src/Pembelian.java
@@ -38,27 +38,27 @@ public class Pembelian {
    
     public static void beliTiket(){
         Scanner sc = new Scanner(System.in);
-        System.out.print(s:"NIK            :");
+        System.out.print("NIK            :");
         Penumpang.setNik(sc.nextLine());
-        System.out.print(s:"NAMA DEPAN     :");
+        System.out.print("NAMA DEPAN     :");
         Penumpang.setNamaDepan(sc.nextLine());
-        System.out.print(s:"NAMA BELAKANG  :");
+        System.out.print("NAMA BELAKANG  :");
         Penumpang.setNamaBelakang(sc.nextLine());
         Penumpang.tampilDafarPenumpang();
 
-        System.out.print(s:"pilih nomor penerbangan");
+        System.out.print("pilih nomor penerbangan");
         pilihPenerbangan = sc.nextInt();
 
         if (pilihPenerbangan > dataPenerbangan.size()) {
-            System.out.println(x:"Penerbangan tidak tersedia");
+            System.out.println("Penerbangan tidak tersedia");
         }
     
     }
 
     public static void main(String[] args) throws Exception {
         dataPenerbangan = new ArrayList<>();
-        dataPenerbangan.add(new Penerbangan(nomorPenerbangan:"GA101", bandaraKeberangkatan:"CGK, Jakarta", bandaraKeberangkatan:"DPS, Bali", "06.30", "08.15", 120000000));
-        dataPenerbangan.add(new Penerbangan(nomorPenerbangan:"QZ202", bandaraKeberangkatan:"SUB, SURABAYA",bandaraKeberangkatan: "KNO, Medan", "09.00", "11.45", 135000000));
+        dataPenerbangan.add(new Penerbangan("GA101", "CGK, Jakarta","DPS, Bali", "06.30", "08.15", 120000000));
+        dataPenerbangan.add(new Penerbangan("QZ202", "SUB, SURABAYA","KNO, Medan", "09.00", "11.45", 135000000));
         
         Scanner sc = new Scanner(System.in);
         int chooseMenu;
@@ -66,11 +66,11 @@ public class Pembelian {
     do { 
         System.out.println();
         System.out.println("======= EAD Ticket Booking System =======");
-        System.out.println(x:"1. Tampilkan Daftar Penerbangan");
-        System.out.println(x:"2. Beli Tiket");
-        System.out.println(x:"3. Tampilkan Pesanan Tiket");
-        System.out.println(x:"4. Exit");
-        System.out.println(x:"Silahkan pilih menu: ");
+        System.out.println("1. Tampilkan Daftar Penerbangan");
+        System.out.println("2. Beli Tiket");
+        System.out.println("3. Tampilkan Pesanan Tiket");
+        System.out.println("4. Exit");
+        System.out.println("Silahkan pilih menu: ");
         chooseMenu = sc.nextInt();
 
         switch (chooseMenu) {
@@ -84,10 +84,10 @@ public class Pembelian {
                 detailTiket();
                 
             case 4:
-                System.out.println(x:"Terima kasih.");
+                System.out.println("Terima kasih.");
                 break;
             default:
-                System.out.println(x:"Menu tidak tersedia.");
+                System.out.println("Menu tidak tersedia.");
         }
     } while (chooseMenu != 4);
     
