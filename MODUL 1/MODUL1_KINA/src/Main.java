64fc6d2b69fdb628d3714544923b31a03d012255
kinal <masayusakinah6@gmail.com>
2024-10-15 08:44:13 +0700

    kina

--- MODUL 1/MODUL1_KINA/src/Main.java
+++ MODUL 1/MODUL1_KINA/src/Main.java
@@ -1,3 +1,27 @@
-public class Main {
+// Don't delete any comments below!!!
+// Todo: Import Scanner
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ManajemenInventaris manajemenInventaris = new ManajemenInventaris();
+        Scanner scanner = new Scanner(System.in);
+        // for (int i = 0; )
+        while (true) {
+            System.out.println("===== Menu Inventaris Makanan EAD =====");
+            System.out.println("1. Tambah Makanan Kering");
+            System.out.println("2. Tambah Makanan Basah");
+            System.out.println("3. Tampilkan Semua Makanan");
+            System.out.println("4. Keluar");
+            System.out.println("Pilih Menu"); 
+            int pilih = scanner.nextInt();
+        }
+           
+        // Todo : Create ManajemenInventaris Object and Scanner
+
+
+
+        // Todo : Create Loop list menu
+    }
 }
--- MODUL 1/MODUL1_KINA/src/ManajemenInventaris.java
+++ MODUL 1/MODUL1_KINA/src/ManajemenInventaris.java
@@ -1,3 +1,70 @@
+// Don't delete any comments below!!!
+// Todo : Import Arraylist and Scanner
+import java.util.ArrayList;
+import java.util.Scanner;
 public class ManajemenInventaris {
+    // Todo : Create ArrayList of MakananKering (daftarMakananKering) and MakananBasah (daftarMakananBasah) to store items
+    ArrayList<MakananBasah> daftarMakananBasah = new ArrayList<>();
+    ArrayList<MakananKering> daftarMakananKering = new ArrayList<>();
 
+   
+        // Todo : Create input for Nama Makanan, Jumlah Makanan, Harga Makanan, and Brand Makanan
+        public void tambahMakananKering() { 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nama Makanan: ");
+        String nama = (scanner.nextLine());
+
+        System.out.println("Jumlah: ");
+        int jumlah = (scanner.nextInt());
+
+        System.out.print("Harga: ");
+        double harga = (scanner.nextDouble());
+
+        System.out.println("brand: ");
+        String brand = (scanner.nextLine());
+
+        // Todo : Create a new object for MakananKering
+        MakananKering makananKering = new MakananKering( nama,  jumlah,  harga,  brand);
+        
+        // Todo : Create Print Notifitaction "Makanan kering berhasil ditambahkan"
+        System.out.println("Makanan Kering berhasil ditambahkan!");
+    }
+
+    public void tambahMakananBasah() {
+        // Todo : Create input for Nama Makanan, Jumlah Makanan, Harga Makanan, and Bahan Makanan
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nama Makanan: ");
+        String nama = (scanner.nextLine());
+
+        System.out.println("Jumlah: ");
+        int jumlah = (scanner.nextInt());
+
+        System.out.print("Harga: ");
+        double harga = (scanner.nextDouble());
+
+        System.out.println("bahan: ");
+        String bahan= (scanner.nextLine());
+
+        // Todo : Create a new object for MakananBasah
+        MakananBasah makananBasah = new MakananBasah( nama,  jumlah,  harga,  bahan);
+        // Todo :Create Print Notifitaction "Makanan Basah berhasil ditambahkan"
+        System.out.println("Makanan Basah berhasil ditambahkan!");
+    }
+    
+    
+    public void tampilkanSemuaMakanan() {
+        if (daftarMakananKering.isEmpty() && daftarMakananBasah.isEmpty()) {
+            // Todo : Create Print Notification "Tidak ada makanan disini"
+            System.out.println("Maaf nmakanan tidak ada");
+        } else {
+            // Todo : Create print notification for Makanan Kering  
+            for 
+            }
+            // Todo : Create print notification for  Makanan Basah
+            for 
+            }
+        }
+    }
 }
